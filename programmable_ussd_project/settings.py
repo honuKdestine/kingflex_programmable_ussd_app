6f21f6c647921b25cda88ce896f437aa32ab9e3f
honuKdestine <honudestine933@gmail.com>
2025-11-12 15:53:54 +0000

    Deploy app to railway

--- programmable_ussd_project/settings.py
+++ programmable_ussd_project/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "False") == "True"
+DEBUG = os.getenv("DEBUG", "True") == "False"
 POS_SALES_ID = os.getenv("POS_SALES_ID")
 
 # ALLOWED_HOSTS = [
