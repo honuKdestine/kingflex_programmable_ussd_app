87b597141821ca9ad7ab22a67d9183ed13a8cbad
honuKdestine <honudestine933@gmail.com>
2025-11-14 15:07:20 +0000

    on fly

--- programmable_ussd_project/settings.py
+++ programmable_ussd_project/settings.py
@@ -20,6 +20,10 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://ussd-project-kingflex.fly.dev",
+]
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
